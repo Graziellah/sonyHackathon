ac74dc3d3fb9f2304ca6a41040ccc7218c16794f
SWAMINATHAN Mickael <smickael@student.42.us.org>
2019-03-19 14:26:15 +0100

    Correction.js

---
+++ src/Correction.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const Correction = () => (
+  <div>
+    <br/><br/><br/><br/><br/><br/>
+    <div>
+    <p className="question">???</p>
+    <Button inverted color='red'>
+      Mauvais
+    </Button>
+    <Button inverted color='orange'>
+      Moyen
+    </Button>
+    <Button inverted color='yellow'>
+      Bon
+    </Button>
+    <Button inverted color='green'>
+      Excellent
+    </Button>
+    </div>
+
+    <div>
+    <br/><br/><br/>
+    <p className="question">???</p>
+    <Button inverted color='red'>
+      Mauvais
+    </Button>
+    <Button inverted color='orange'>
+      Moyen
+    </Button>
+    <Button inverted color='yellow'>
+      Bon
+    </Button>
+    <Button inverted color='green'>
+      Excellent
+    </Button>
+    </div>
+
+    <div>
+    <br/><br/><br/>
+    <p className="question">???</p>
+    <Button inverted color='red'>
+      Mauvais
+    </Button>
+    <Button inverted color='orange'>
+      Moyen
+    </Button>
+    <Button inverted color='yellow'>
+      Bon
+    </Button>
+    <Button inverted color='green'>
+      Excellent
+    </Button>
+    </div>
+
+    <div>
+    <br/><br/><br/>
+    <p className="question">???</p>
+    <Button inverted color='red'>
+      Mauvais
+    </Button>
+    <Button inverted color='orange'>
+      Moyen
+    </Button>
+    <Button inverted color='yellow'>
+      Bon
+    </Button>
+    <Button inverted color='green'>
+      Excellent
+    </Button>
+    </div>
+  </div>
+)
+
+export default Correction
